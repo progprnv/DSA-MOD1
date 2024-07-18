38b52cb88cff5dc61efe3a6d0555bb6c2ae6fce2
progprnv <pranavpranav29907@gmail.com>
2024-07-19 01:11:33 +0530

    pr

---
+++ search.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void search(int a,int n,intx);
+void main()
+{ int a[20],n,x,i;
+printf("enter the no of arrays \n");
+scanf("%d",&n);
+printf("enter the elements to array");
+for(i=0;i<n;i++)
+{scanf("%d",&a[i]);
+}
+}
