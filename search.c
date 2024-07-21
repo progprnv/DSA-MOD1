ab73efd862a05e929dcc75b9eef8a4310dd66cc0
progprnv <145828371+progprnv@users.noreply.github.com>
2024-07-21 11:58:36 +0530

    Update search.c

--- search.c
+++ search.c
@@ -1,11 +1,42 @@
+
 #include <stdio.h>
-void search(int a,int n,intx);
-void main()
-{ int a[20],n,x,i;
-printf("enter the no of arrays \n");
-scanf("%d",&n);
-printf("enter the elements to array");
-for(i=0;i<n;i++)
-{scanf("%d",&a[i]);
+
+void search(int a[], int n, int x);
+
+void main() {
+    int a[20], n, x, i;
+
+    printf("Enter size of array\n");
+    scanf("%d", &n);
+
+    printf("Enter elements of array\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Entered array is\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    search(a, n, x);
 }
+
+void search(int a[], int n, int x) {
+    int i, flag = 0, pos;
+
+    printf("Enter element to be searched\n");
+    scanf("%d", &x);
+
+    for (i = 0; i < n; i++) {
+        if (a[i] == x) {
+            flag = 1;
+            pos = i + 1;
+            printf("The element is in position %d\n", pos);
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("\nNo element found");
 }
