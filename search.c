23f1189bf8f832ecc1d0d2d94caa834ce3a3fbe7
progprnv <pranavpranav29907@gmail.com>
2024-07-21 22:04:43 +0530

    search prgm

--- search.c
+++ search.c
@@ -1,11 +1,41 @@
 #include <stdio.h>
-void search(int a,int n,intx);
-void main()
-{ int a[20],n,x,i;
-printf("enter the no of arrays \n");
-scanf("%d",&n);
-printf("enter the elements to array");
-for(i=0;i<n;i++)
-{scanf("%d",&a[i]);
+void search(int a[],int n,int x);
+int void()
+{
+	int a[20],n,x,i;
+	printf("enter the size of elements");
+	scanf("%d", &n);
+	
+	printf("enter elements of array:");
+	for(i=0;i<n;i++)
+	{
+	 scanf("%d",&a[i]); 
+ }
+ 
+ print("entered array;");
+ for(i=0;i<n;i++)
+ { printf("%d \n",&a[i]);
+ }
+ 
+ search(a,n,x);
 }
+
+void search(int a[],int n,int x)
+{
+	int i,flag=0;pos;
+	
+	printf("enter element to be searched \n");
+	scanf("%d",&x);
+	
+	{ if(a[i]==x)
+		{
+			flag=1;
+			pos=i+1;
+			printf("the element is in position %d \n",pos);
+			break;
+		}
+	}
+	if (flag==0) {
+		printf("element not found");
+	}
 }
