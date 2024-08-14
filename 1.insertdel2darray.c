c2bdca25be131fb4bb201dab2d805328d00381b6
progprnv <pranavpranav29907@gmail.com>
2024-08-14 21:11:34 +0530

    new

---
+++ 1.insertdel2darray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+
+{
+
+
+int i,j,n,A[10],ch,x,p,max;
+char a;
+printf("enter the numbers");
+scanf("%d",&n);
+printf("enter the elements");
+for(i=0;i<n;i++);
+
+{
+	scanf("%d",&A[i]);
+}
+
+do
+
+{
+	printf("enter 
