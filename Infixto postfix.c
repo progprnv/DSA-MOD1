fe5c211823441a052a1f0221186e0a60b350cd01
progprnv <145828371+progprnv@users.noreply.github.com>
2024-10-14 14:48:50 +0530

    Update Infixto postfix.c

--- Infixto postfix.c
+++ Infixto postfix.c
@@ -1,135 +1,73 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-# define max 15
- char infix[max],postfix[max];
- int stack[max];
- void itop();
- void push(char);
- char pop();
- int prec(char);
- void eval();
- int top=0;
 
-void main()
-{       
-         printf("Enter infix expression ");
-         scanf("%s",infix);
-         itop();  //infix to postfix
-         eval(); //postfix evaluation
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 20
+char postfix[MAX];
+int top = -1;
+void push(char x) 
+{
+  if (top < MAX - 1) 
+  {
+   top = top + 1;
+   postfix[top] = x;
   }
-
-void itop()
-{
-     int i,p=0,precedence,len;
-     char next;
-     stack[top]='#';
-     len=strlen(infix);
-     infix[len]='#';
-     for(i=0;infix[i]!='#';i++)
-       {
-           switch(infix[i])
-           {
-	           case '(': push(infix[i]);
-	                     break;
-	           case ')': while((next=pop())!='(')
-		                	postfix[p++]=next;
-			             break;
-               case '+' :
-	           case '-' :
-	           case '*' :
-               case '/' :
-               case '%' :
-               case '^' :
-		                 precedence=prec(infix[i]);
-		                 while(stack[top]!='#' && precedence<=prec(stack[top])) //in stack priority >= incoming priority
-		                     postfix[p++]=pop();
-		                     push(infix[i]);
-		                     break;
-	           default :     postfix[p++]=infix[i]; //operands
-
-            }
-
-    }
-while(stack[top]!='#') //Remaining elements in stack
-      postfix[p++]=pop();
-      postfix[p]='\0';
-      printf("%s",postfix);
-  
 }
-
-int prec(char symbol)
+char pop() 
 {
-  switch(symbol)
+  if (top == -1) 
+  {
+    return -1;
+  }
+  else 
    {
-	case '(': return 0;
-
-	case '+' :
-	case '-' : return 1;
-	case '*' :
-	case '/' :
-	case '%' : return 2;
-	case '^' : return 3;
-
+    return postfix[top--];
    }
 }
-void push( char symbol)
-{
-  if(top>max)
-  {
-    printf("stack full");
-    exit(0);
-  }
-  else
-  {
-   stack[++top]=symbol;
-  }
-}
-
-char pop()
-{
-  if(top==-1)
-  {
-   printf("stack underflow");
-   exit(0);
-  }
-  else
-  {
-   return(stack[top--]);
-  }
-}
-
-void eval()
-{
- int l,i,a,b,res;
- l=strlen(postfix);
- postfix[l]='#';
- for(i=0;postfix[i]!='#';i++)
+int priority(char x)
  {
-   if(postfix[i]<='9' && postfix[i]>='0')
-   {
-     push (postfix[i]-48);
-   }
-   else
-   {
-     a=pop();
-     b=pop();
-     switch(postfix[i])
-
-     {
-	case '+' : push(b+a); break;
-	case '-' : push(b-a); break;
-	case '*' : push(b*a); break;
-	case '/' : push(b/a); break;
-	case '%' : push(b%a); break;
-	case '^' : push(pow(b,a)); break;
-
-
-     }
-
-   }
+   if (x == '(') return 0;
+   if (x == '+' || x == '-') return 1;
+   if (x == '*' || x == '/') return 2;
+   if (x == '^') return 3;
+   return -1;
  }
- res=pop();
- printf("\n eval res : %d", res);
+ int main()
+{
+ char infix[MAX], *e, x;
+ printf("Enter the expression: ");
+ scanf("%s", infix);
+ e = infix;
+ while (*e != '\0')
+  {
+    if (isalnum(*e))
+     {
+       printf("%c", *e);
+     } 
+      else if (*e == '(')
+       {
+        push(*e);
+       } 
+        else if (*e == ')')
+         {
+          while ((x = pop()) != '(')  
+          {
+           if (x == -1) break;
+           printf("%c", x);
+          }
+         } 
+           else
+            {
+             while (top != -1 && priority(postfix[top]) >= priority(*e))
+              {
+               printf("%c", pop());
+              }
+              push(*e);
+           }
+            e++;
+  }
+   while (top != -1)
+    {
+      printf("%c", pop());
+    }
+    return 0;
 }
